998d51ac740ac1702b9a844e19f1ab733e1f5dfc
514870 <514870@01HW442795.India.TCS.com>
2016-03-31 20:25:07 +0530

    code change

--- DBLogViewer/src/com/tcs/webservices/JobLogServ.java
+++ DBLogViewer/src/com/tcs/webservices/JobLogServ.java
@@ -4,14 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.persistence.Entity;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 
 @WebService()
@@ -78,64 +81,93 @@ public class JobLogServ {
     @Path("retrive")
     @Produces("text/plain")
     @WebMethod(operationName = "retrive")
-	public String retrive() {
+	public String retrive(@QueryParam("runid") int runID,@QueryParam("etl_date") String etlDate,@QueryParam("entity") String entity) {
 		
 		ResultSet rs = null;
         String strHeader = ""; 
         String strData = "";
         String finalStr = "";
+        
+        
+        
 		try 
         {
-            Class.forName("com.mysql.jdbc.Driver");
+            
+			if(!etlDate.equals("")) {
+	        	
+	        	DateFormat newEtlDate = new SimpleDateFormat("MM/dd/yyyy"); 
+	        	Date dt = newEtlDate.parse(etlDate);
+	        	
+	        	SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
+	        	etlDate = dt1.format(dt);
+	        	
+	        }
+			
+			Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://192.168.225.109:3306/wso2devregdb", "devdbusr", "devdbusr");
 
-            String query = "SELECT job_id, runid, entity, run_mode, ExtractStart, ExtractEnd, S3LoadStart, S3LoadEnd, RedShiftLoadStart, RedShiftLoadEnd, job_status, subsidiary_id FROM job_log;";
+            //String query = "SELECT job_id, runid, entity, run_mode, ExtractStart, ExtractEnd, S3LoadStart, S3LoadEnd, RedShiftLoadStart, RedShiftLoadEnd, job_status, subsidiary_id FROM job_log;";
+            
+            String query = "select job_id, runid, entity, run_mode, ExtractStart, ExtractEnd, S3LoadStart, S3LoadEnd, RedShiftLoadStart, RedShiftLoadEnd, job_status, subsidiary_id from job_log a"
+            		+ " where a.runid= coalesce(?,a.runid)"
+            		+ "and DATE_FORMAT(a.ExtractStart,'%Y-%m-%d') = coalesce(?,DATE_FORMAT(a.ExtractStart,'%Y-%m-%d'))"
+            		+ "and a.entity = coalesce(?,a.entity)";
+            
             PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, (runID == 0  ? null:runID));
+            st.setString(2, (etlDate.trim() == ""  ? null:etlDate));
+            st.setString(3,(entity == "" ? null:entity));
+            //System.out.println("After : " + st.toString());
             rs = st.executeQuery();
             strHeader = "{"
             		+ "\"data\": [";
             int i = 0;
-            while (rs.next()) 
-            {
-            	if(i == 0) {
-            		strData	= strData	+ "{ "
-            				+ "\"JobId\":" + rs.getInt("job_id") + ","
-            				+ "\"RunId\":" + rs.getInt("runid") + ","
-		            		+ "\"Entity\": \"" + rs.getString("entity") + "\","
-		            		+ "\"Run Mode\": \"" + rs.getString("run_mode") + "\","
-		            		+ "\"ExtractStartTime\": \"" + rs.getTimestamp("ExtractStart") + "\","
-		            		+ "\"ExtractEndTime\": \"" + rs.getTimestamp("ExtractEnd") + "\","
-		            		+ "\"S3LoadStartTime\": \"" + rs.getTimestamp("S3LoadStart") + "\","
-		            		+ "\"S3LoadEndTime\": \"" + rs.getTimestamp("S3LoadEnd") + "\","
-		            		+ "\"RedShiftLoadStart\": \"" + rs.getTimestamp("RedShiftLoadStart") + "\","
-		            		+ "\"RedShiftLoadEnd\": \"" + rs.getTimestamp("RedShiftLoadEnd") + "\","
-		            		+ "\"Job Status\": \"" + rs.getString("job_status") + "\","
-		            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
-		            		+ "}";
-            	} else {
-            		strData = strData + "," 
-            				+  "{ "
-            				+ "\"JobId\":" + rs.getInt("job_id") + ","
-            				+ "\"RunId\":" + rs.getInt("runid") + ","
-		            		+ "\"Entity\": \"" + rs.getString("entity") + "\","
-		            		+ "\"Run Mode\": \"" + rs.getString("run_mode") + "\","
-		            		+ "\"ExtractStartTime\": \"" + rs.getTimestamp("ExtractStart") + "\","
-		            		+ "\"ExtractEndTime\": \"" + rs.getTimestamp("ExtractEnd") + "\","
-		            		+ "\"S3LoadStartTime\": \"" + rs.getTimestamp("S3LoadStart") + "\","
-		            		+ "\"S3LoadEndTime\": \"" + rs.getTimestamp("S3LoadEnd") + "\","
-		            		+ "\"RedShiftLoadStart\": \"" + rs.getTimestamp("RedShiftLoadStart") + "\","
-		            		+ "\"RedShiftLoadEnd\": \"" + rs.getTimestamp("RedShiftLoadEnd") + "\","
-		            		+ "\"Job Status\": \"" + rs.getString("job_status") + "\","
-		            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
-		            		+ "}";
-            	         		
-            	}
-            	
-            	i++;
-            }
+            if(rs.next()) {
+            	rs.beforeFirst();            
+	            while (rs.next()) 
+	            {
+	            	if(i == 0) {
+	            		strData	= strData	+ "{ "
+	            				+ "\"JobId\":" + rs.getInt("job_id") + ","
+	            				+ "\"RunId\":" + rs.getInt("runid") + ","
+			            		+ "\"Entity\": \"" + rs.getString("entity") + "\","
+			            		+ "\"Run Mode\": \"" + rs.getString("run_mode") + "\","
+			            		+ "\"ExtractStartTime\": \"" + rs.getTimestamp("ExtractStart") + "\","
+			            		+ "\"ExtractEndTime\": \"" + rs.getTimestamp("ExtractEnd") + "\","
+			            		+ "\"S3LoadStartTime\": \"" + rs.getTimestamp("S3LoadStart") + "\","
+			            		+ "\"S3LoadEndTime\": \"" + rs.getTimestamp("S3LoadEnd") + "\","
+			            		+ "\"RedShiftLoadStart\": \"" + rs.getTimestamp("RedShiftLoadStart") + "\","
+			            		+ "\"RedShiftLoadEnd\": \"" + rs.getTimestamp("RedShiftLoadEnd") + "\","
+			            		+ "\"Job Status\": \"" + rs.getString("job_status") + "\","
+			            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
+			            		+ "}";
+	            	} else {
+	            		strData = strData + "," 
+	            				+  "{ "
+	            				+ "\"JobId\":" + rs.getInt("job_id") + ","
+	            				+ "\"RunId\":" + rs.getInt("runid") + ","
+			            		+ "\"Entity\": \"" + rs.getString("entity") + "\","
+			            		+ "\"Run Mode\": \"" + rs.getString("run_mode") + "\","
+			            		+ "\"ExtractStartTime\": \"" + rs.getTimestamp("ExtractStart") + "\","
+			            		+ "\"ExtractEndTime\": \"" + rs.getTimestamp("ExtractEnd") + "\","
+			            		+ "\"S3LoadStartTime\": \"" + rs.getTimestamp("S3LoadStart") + "\","
+			            		+ "\"S3LoadEndTime\": \"" + rs.getTimestamp("S3LoadEnd") + "\","
+			            		+ "\"RedShiftLoadStart\": \"" + rs.getTimestamp("RedShiftLoadStart") + "\","
+			            		+ "\"RedShiftLoadEnd\": \"" + rs.getTimestamp("RedShiftLoadEnd") + "\","
+			            		+ "\"Job Status\": \"" + rs.getString("job_status") + "\","
+			            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
+			            		+ "}";
+	            	         		
+	            	}
+	            	
+	            	i++;
+	            }
             
-            strData = strData + "] }";
-            finalStr = strHeader + strData;
+	            strData = strData + "] }";
+	            finalStr = strHeader + strData;
+            } else {
+            	finalStr = "No Data Present";
+            }
             
         }
 		
@@ -143,7 +175,10 @@ public class JobLogServ {
         {
             System.out.println(e.getMessage());
         }   
+        
         return finalStr;
 		
 	}
+
+	
 }
