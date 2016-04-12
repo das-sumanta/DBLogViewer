4433b5ca2c5454004dfdd0e26b24211bad2f98bc
514870 <514870@01HW442795.India.TCS.com>
2016-04-12 21:00:15 +0530

    Code Updated

--- DBLogViewer/src/com/tcs/webservices/JobLogServ.java
+++ DBLogViewer/src/com/tcs/webservices/JobLogServ.java
@@ -27,7 +27,7 @@ public class JobLogServ {
     @Path("retrivejoblog")
     @Produces("text/plain")
     @WebMethod(operationName = "retrivejoblog")
-	public String retrivejoblog(@QueryParam("runid") String runID,@QueryParam("etl_date") String etlDate,@QueryParam("entity") String entity) throws SQLException {
+	public String retrivejoblog(@QueryParam("runid") String runID,@QueryParam("etl_date") String etlDate,@QueryParam("tabs") String tabs) throws SQLException {
 		
 		ResultSet rs = null;
 		PreparedStatement st = null;
@@ -63,8 +63,8 @@ public class JobLogServ {
             st = con.prepareStatement(query);
             st.setString(1, (runID != null && runID.length() != 0  ? runID:null));
             st.setString(2, (etlDate != null && etlDate.length() != 0  ? etlDate:null));
-            st.setString(3,(entity != null && entity.length() != 0 ? entity:null));
-            System.out.println("After : " + st.toString());
+            st.setString(3,(tabs != null && tabs.length() != 0 ? tabs:null));
+            
             rs = st.executeQuery();
             strHeader = "{"
             		+ "\"data\": [";
@@ -112,6 +112,7 @@ public class JobLogServ {
             
 	            strData = strData + "] }";
 	            finalStr = strHeader + strData;
+	            finalStr = finalStr.replaceAll("null", "-");
             } else {
             	finalStr = "No Data Present";
             }
@@ -139,43 +140,46 @@ public class JobLogServ {
     @Path("retrivemsglog")
     @Produces("text/plain")
     @WebMethod(operationName = "retrivemsglog")
-	public String retrivemsglog(@QueryParam("runid") String runID,@QueryParam("etl_date") String etlDate,@QueryParam("entity") String entity) throws SQLException {
+	
+	public String retrivemsglog(@QueryParam("runid") String runID,@QueryParam("etl_date") String etlDate,@QueryParam("tabs") String tables) throws SQLException {
 		
 		ResultSet rs = null;
-		PreparedStatement st=null;
-		Connection con=null;
+		PreparedStatement st = null;
+		Connection con = null;
         String strHeader = ""; 
         String strData = "";
         String finalStr = "";
         
         Properties prop = new Properties();
         InputStream propertiesInputStream = null;
-        
-		try 
+		
+        try 
         {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        
+        	ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 			propertiesInputStream = classLoader.getResourceAsStream("com/tcs/webservices/dbprop.properties");
 			prop.load(propertiesInputStream);
 			propertiesInputStream.close();
 			
-			Class.forName("com.mysql.jdbc.Driver");
-            //con = DriverManager.getConnection("jdbc:mysql://192.168.225.109:3306/wso2devregdb", "devdbusr", "devdbusr");
-            con = DriverManager.getConnection(prop.getProperty("LogDBURL"), prop.getProperty("LogDBUID"), prop.getProperty("LogDBPwd"));
-            //String query = "SELECT job_id, runid, entity, run_mode, ExtractStart, ExtractEnd, S3LoadStart, S3LoadEnd, RedShiftLoadStart, RedShiftLoadEnd, job_status, subsidiary_id FROM job_log;";
-            
-            String query = "select message_id, runid, message_desc, target_table, message_stage, message_type, message_timestamp, subsidiary_id from message_log a"
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection(prop.getProperty("LogDBURL"), prop.getProperty("LogDBUID"), prop.getProperty("LogDBPwd"));
+        	
+        	String query = "select message_id, runid, message_desc, target_table, message_stage, message_type, message_timestamp, subsidiary_id from message_log a"
             		+ " where a.runid= coalesce(?,a.runid)"
             		+ "and DATE_FORMAT(a.message_timestamp,'%Y-%m-%d') = coalesce(?,DATE_FORMAT(a.message_timestamp,'%Y-%m-%d'))"
             		+ "and a.target_table = coalesce(?,a.target_table)";
+        	
+        	//String query = "select * from message_log where runid=66";
             
             st = con.prepareStatement(query);
             st.setString(1, (runID != null && runID.length() != 0  ? runID:null));
             st.setString(2, (etlDate != null && etlDate.length() != 0 ? etlDate:null));
-            st.setString(3,(entity != null && entity.length() != 0 ? entity:null));
-            System.out.println("After : " + st.toString());
+            st.setString(3,(tables != null && tables.length() != 0 ? tables:null));
+            
             rs = st.executeQuery();
             strHeader = "{"
             		+ "\"data\": [";
+            
             int i = 0;
             if(rs.next()) {
             	rs.beforeFirst();            
@@ -212,30 +216,133 @@ public class JobLogServ {
             
 	            strData = strData + "] }";
 	            finalStr = strHeader + strData;
+	            finalStr = finalStr.replaceAll("(\\r|\\n|\\r\\n)+", "");
+	            finalStr = finalStr.replaceAll("null", "-");
             } else {
             	finalStr = "No Data Present";
             }
             
         }
-		
-		catch (Exception e) 
-        {
-            System.out.println(e.getMessage());
-        }   
+        catch (Exception e) 
+      {
+          System.out.println(e.getMessage());
+      }   
 		finally {
-        	
-        	if(st != null){
-        		st.close();
-        	}
-        	
-        	if(con != null){
-        		con.close();
-        	}
-        }
-        
-        return finalStr;
-		
+      	
+      	if(st != null){
+      		st.close();
+      	}
+      	
+      	if(con != null){
+      		con.close();
+      	}
+      }
+		return finalStr;
 	}
-
-	
 }
+	
+	
+//	@GET
+//    @Path("retrivemsglog")
+//    @Produces("text/plain")
+//    @WebMethod(operationName = "retrivemsglog")
+//	public String retrivemsglog(@QueryParam("runid") String runID,@QueryParam("etl_date") String etlDate,@QueryParam("entity") String entity) throws SQLException {
+//		
+//		ResultSet rs = null;
+//		PreparedStatement st=null;
+//		Connection con=null;
+//        String strHeader = ""; 
+//        String strData = "";
+//        String finalStr = "";
+//        
+//        Properties prop = new Properties();
+//        InputStream propertiesInputStream = null;
+//        
+//		try 
+//        {
+//			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//			propertiesInputStream = classLoader.getResourceAsStream("com/tcs/webservices/dbprop.properties");
+//			prop.load(propertiesInputStream);
+//			propertiesInputStream.close();
+//			
+//			Class.forName("com.mysql.jdbc.Driver");
+//            //con = DriverManager.getConnection("jdbc:mysql://192.168.225.109:3306/wso2devregdb", "devdbusr", "devdbusr");
+//            con = DriverManager.getConnection(prop.getProperty("LogDBURL"), prop.getProperty("LogDBUID"), prop.getProperty("LogDBPwd"));
+//            //String query = "SELECT job_id, runid, entity, run_mode, ExtractStart, ExtractEnd, S3LoadStart, S3LoadEnd, RedShiftLoadStart, RedShiftLoadEnd, job_status, subsidiary_id FROM job_log;";
+//            
+//            String query = "select message_id, runid, message_desc, target_table, message_stage, message_type, message_timestamp, subsidiary_id from message_log a"
+//            		+ " where a.runid= coalesce(?,a.runid)"
+//            		+ "and DATE_FORMAT(a.message_timestamp,'%Y-%m-%d') = coalesce(?,DATE_FORMAT(a.message_timestamp,'%Y-%m-%d'))"
+//            		+ "and a.target_table = coalesce(?,a.target_table)";
+//            
+//            st = con.prepareStatement(query);
+//            st.setString(1, (runID != null && runID.length() != 0  ? runID:null));
+//            st.setString(2, (etlDate != null && etlDate.length() != 0 ? etlDate:null));
+//            st.setString(3,(entity != null && entity.length() != 0 ? entity:null));
+//            System.out.println("After : " + st.toString());
+//            rs = st.executeQuery();
+//            strHeader = "{"
+//            		+ "\"data\": [";
+//            int i = 0;
+//            if(rs.next()) {
+//            	rs.beforeFirst();            
+//	            while (rs.next()) 
+//	            {
+//	            	if(i == 0) {
+//	            		strData	= strData	+ "{ "
+//	            				+ "\"MessageId\":" + rs.getInt("message_id") + ","
+//	            				+ "\"RunId\":" + rs.getInt("runid") + ","
+//			            		+ "\"Message Desc\": \"" + rs.getString("message_desc") + "\","
+//			            		+ "\"Target Table\": \"" + rs.getString("target_table") + "\","
+//			            		+ "\"Message Stage\": \"" + rs.getString("message_stage") + "\","
+//			            		+ "\"Message Type\": \"" + rs.getString("message_type") + "\","
+//			            		+ "\"Message TimeStamp\": \"" + rs.getTimestamp("message_timestamp") + "\","
+//			            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
+//			            		+ "}";
+//	            	} else {
+//	            		strData = strData + "," 
+//	            				+  "{ "
+//	            				+ "\"MessageId\":" + rs.getInt("message_id") + ","
+//	            				+ "\"RunId\":" + rs.getInt("runid") + ","
+//			            		+ "\"Message Desc\": \"" + rs.getString("message_desc") + "\","
+//			            		+ "\"Target Table\": \"" + rs.getString("target_table") + "\","
+//			            		+ "\"Message Stage\": \"" + rs.getString("message_stage") + "\","
+//			            		+ "\"Message Type\": \"" + rs.getString("message_type") + "\","
+//			            		+ "\"Message TimeStamp\": \"" + rs.getTimestamp("message_timestamp") + "\","
+//			            		+ "\"Subsidiary ID\": \"" + rs.getString("subsidiary_id") + "\""
+//			            		+ "}";
+//	            	         		
+//	            	}
+//	            	
+//	            	i++;
+//	            }
+//            
+//	            strData = strData + "] }";
+//	            finalStr = strHeader + strData;
+//            } else {
+//            	finalStr = "No Data Present";
+//            }
+//            
+//        }
+//		
+//		catch (Exception e) 
+//        {
+//            System.out.println(e.getMessage());
+//        }   
+//		finally {
+//        	
+//        	if(st != null){
+//        		st.close();
+//        	}
+//        	
+//        	if(con != null){
+//        		con.close();
+//        	}
+//        }
+//        
+//        return finalStr;
+		
+//	}
+//
+//	
+//}
